870aa94da53cb09c79c4c50b3889319f36232a82
Siwen Yu <yusiwen@gmail.com>
2023-05-23 21:23:28 +0800

    fix environments and scripts

--- containers/compile/entry.sh
+++ containers/compile/entry.sh
@@ -1,73 +1,74 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 escape() {
-  local tmp=`echo $1 | sed 's/[^a-zA-Z0-9\s:]/\\\&/g'`
-  echo "$tmp"
+   local tmp
+   tmp=$(echo $1 | sed 's/[^a-zA-Z0-9\s:]/\\\&/g')
+   echo "$tmp"
 }
 
 if [ ! -d "$SOURCE_PREFIX" ]; then
-   cd /opt/etc
-   git clone $GIT_URL_SOURCE $SOURCE_PREFIX
+   git clone --depth 1 --branch master "$GIT_URL_SOURCE" "$SOURCE_PREFIX"
+   git config pull.rebase true
 else
-   cd $SOURCE_PREFIX
-   git pull
+   cd "$SOURCE_PREFIX" || return
+   git pull origin master
 fi
 
 if [ ! -d "$INSTALL_PREFIX/logs" ]; then
-   mkdir -p $INSTALL_PREFIX/logs
+   mkdir -p "$INSTALL_PREFIX"/logs
 fi
 
 if [ ! -d "$INSTALL_PREFIX/etc" ]; then
-   mkdir -p $INSTALL_PREFIX/etc
+   mkdir -p "$INSTALL_PREFIX"/etc
 fi
 
 if [ ! -d "$SOURCE_PREFIX/build" ]; then
-   mkdir -p $SOURCE_PREFIX/build
+   mkdir -p "$SOURCE_PREFIX"/build
 fi
 
-cd $SOURCE_PREFIX/build
+cd "$SOURCE_PREFIX"/build || return
 
-cmake .. -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_C_COMPILER=$CMAKE_C_COMPILER -DCMAKE_CXX_COMPILER=$CMAKE_CXX_COMPILER -DSCRIPTS=$SCRIPTS -DWITH_WARNINGS=$WARNINGS -DTOOLS=$EXTRACTORS -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS
+cmake .. -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DCMAKE_C_COMPILER="$CMAKE_C_COMPILER" -DCMAKE_CXX_COMPILER="$CMAKE_CXX_COMPILER" -DSCRIPTS="$SCRIPTS" -DWITH_WARNINGS="$WARNINGS" -DTOOLS="$EXTRACTORS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS"
 
-make clean
+make -j "$(nproc)"
 
-if [ $MAKE_INSTALL -eq 1 ];then
+if [ "$MAKE_INSTALL" -eq 1 ]; then
    echo "Make install"
-   make -j $(nproc) install
+   make install
+
+   if [ ! -f "$CONFIG_PATH/authserver.conf" ]; then
+      echo "updating authserver.conf files"
+      cp "$CONFIG_PATH"/authserver.conf.dist "$CONFIG_PATH"/authserver.conf
+
+      sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" "$CONFIG_PATH"/authserver.conf
+      sed -i -e "/LoginDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;auth\"/" "$CONFIG_PATH"/authserver.conf
+   fi
+
+   if [ ! -f "$CONFIG_PATH/worldserver.conf" ]; then
+      echo "updating worldserver.conf files"
+      cp "$CONFIG_PATH"/worldserver.conf.dist "$CONFIG_PATH"/worldserver.conf
+
+      sed -i -e "/DataDir =/ s/= .*/= $(escape $DATA_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
+      sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
+
+      sed -i -e "/LoginDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;auth\"/" "$CONFIG_PATH"/worldserver.conf
+      sed -i -e "/WorldDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;world\"/" "$CONFIG_PATH"/worldserver.conf
+      sed -i -e "/CharacterDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;characters\"/" "$CONFIG_PATH"/worldserver.conf
+      sed -i -e "/ArchiveDatabaseInfo   =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;archive\"/" "$CONFIG_PATH"/worldserver.conf
+      sed -i -e "/FusionCMSDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;fusion\"/" "$CONFIG_PATH"/worldserver.conf
+
+      sed -i -e "/GameType =/ s/= .*/= $(escape $GAME_TYPE)/" "$CONFIG_PATH"/worldserver.conf
+      sed -i -e "/RealmZone =/ s/= .*/= $(escape $REALM_ZONE)/" "$CONFIG_PATH"/worldserver.conf
+      sed -i -e "/Motd =/ s/= .*/= \"$(escape $MOTD_MSG)\"/" "$CONFIG_PATH"/worldserver.conf
+
+      sed -i -e "/Ra.Enable =/ s/= .*/= $(escape $RA_ENABLE)/" "$CONFIG_PATH"/worldserver.conf
+
+      sed -i -e "/SOAP.Enabled =/ s/= .*/= $(escape $SOAP_ENABLE)/" "$CONFIG_PATH"/worldserver.conf
+      sed -i -e "/SOAP.IP =/ s/= .*/= $(escape $SOAP_IP)/" "$CONFIG_PATH"/worldserver.conf
+   fi
 fi
 
-if [ ! -f "/opt/server/etc/authserver.conf" ]; then
-   echo "updating authserver.conf files"
-   cp /opt/server/etc/authserver.conf.dist /opt/server/etc/authserver.conf
-
-   sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/authserver.conf
-   sed -i -e "/LoginDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;auth\"/" $CONFIG_PATH/authserver.conf
-fi
-
-if [ ! -f "/opt/server/etc/worldserver.conf" ]; then
-   echo "updating worldserver.conf files"
-   cp /opt/server/etc/worldserver.conf.dist /opt/server/etc/worldserver.conf
-
-   sed -i -e "/DataDir =/ s/= .*/= $(escape $DATA_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
-   sed -i -e "/LogsDir =/ s/= .*/= $(escape $LOGS_DIR_PATH)/" $CONFIG_PATH/worldserver.conf
-
-   sed -i -e "/LoginDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;auth\"/" $CONFIG_PATH/worldserver.conf
-   sed -i -e "/WorldDatabaseInfo     =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;world\"/" $CONFIG_PATH/worldserver.conf
-   sed -i -e "/CharacterDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;characters\"/" $CONFIG_PATH/worldserver.conf
-   sed -i -e "/ArchiveDatabaseInfo   =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;archive\"/" $CONFIG_PATH/worldserver.conf
-   sed -i -e "/FusionCMSDatabaseInfo =/ s/= .*/= \"$(escape $DB_CONTAINER)\;3306\;$(escape $SERVER_DB_USER)\;$(escape $SERVER_DB_PWD)\;fusion\"/" $CONFIG_PATH/worldserver.conf
-
-   sed -i -e "/GameType =/ s/= .*/= $(escape $GAME_TYPE)/" $CONFIG_PATH/worldserver.conf
-   sed -i -e "/RealmZone =/ s/= .*/= $(escape $REALM_ZONE)/" $CONFIG_PATH/worldserver.conf
-   sed -i -e "/Motd =/ s/= .*/= \"$(escape $MOTD_MSG)\"/" $CONFIG_PATH/worldserver.conf
-
-   sed -i -e "/Ra.Enable =/ s/= .*/= $(escape $RA_ENABLE)/" $CONFIG_PATH/worldserver.conf
-
-   sed -i -e "/SOAP.Enabled =/ s/= .*/= $(escape $SOAP_ENABLE)/" $CONFIG_PATH/worldserver.conf
-   sed -i -e "/SOAP.IP =/ s/= .*/= $(escape $SOAP_IP)/" $CONFIG_PATH/worldserver.conf
-fi
-
-if [ $MAKE_INSTALL -eq 0 ]; then
+if [ "$MAKE_INSTALL" -eq 0 ]; then
    echo "Open shell"
    /bin/bash
 fi
--- containers/mariadb/entry.sh
+++ containers/mariadb/entry.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 echo "Starting Initialization of CMaNGOS DB..."
 
 echo "Check database sql files"
 
-if [ ! -d "/opt/etc/pandaria_5.4.8" ]; then
-	git clone https://github.com/alexkulya/pandaria_5.4.8 /opt/etc/pandaria_5.4.8
+if [ ! -d "$SOURCE_PREFIX" ]; then
+	git clone --depth 1 --branch master "$GIT_URL_SOURCE" "$SOURCE_PREFIX"
+	git config pull.rebase true
 else
-	cd /opt/etc/pandaria_5.4.8
+    cd "$SOURCE_PREFIX" || return
+	git config --global --add safe.directory /opt/build/pandaria_5.4.8
 	git pull
 	cd /
 fi
@@ -38,21 +40,25 @@ mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON world.* t
 mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD -e "FLUSH PRIVILEGES;"
 
 echo "Populate database"
-mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD archive < /opt/etc/pandaria_5.4.8/sql/base/archive.sql
-mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD auth < /opt/etc/pandaria_5.4.8/sql/base/auth.sql
-mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD characters < /opt/etc/pandaria_5.4.8/sql/base/characters.sql
-mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD fusion < /opt/etc/pandaria_5.4.8/sql/base/fusion.sql
+unzip "$SOURCE_PREFIX"/sql/base/auth_*.zip -d /tmp
+unzip "$SOURCE_PREFIX"/sql/base/characters_*.zip -d /tmp
+unzip "$SOURCE_PREFIX"/sql/base/world_*.zip -d /tmp
 
-7z e /opt/etc/pandaria_5.4.8/sql/base/world.7z -o/tmp
-mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD world < /tmp/world.sql
+mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD auth < /tmp/auth_*.sql
+mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD characters < /tmp/characters_*.sql
+mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD world < /tmp/world_*.sql
+echo "Base sql files initialized"
 
-cat /opt/etc/pandaria_5.4.8/sql/updates/auth\/old/*.sql > /tmp/auth.sql
-cat /opt/etc/pandaria_5.4.8/sql/updates/characters/*.sql > /tmp/characters.sql
-cat /opt/etc/pandaria_5.4.8/sql/updates/world/*.sql > /tmp/world.sql
+cat "$SOURCE_PREFIX"/sql/updates/auth/*.sql > /tmp/auth.sql
+cat "$SOURCE_PREFIX"/sql/updates/characters/*.sql > /tmp/characters.sql
+cat "$SOURCE_PREFIX"/sql/updates/world/*.sql > /tmp/world.sql
+cat "$SOURCE_PREFIX"/sql/updates/world/battlepay/*.sql > /tmp/battlepay.sql
 
 mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD auth < /tmp/auth.sql
 mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD characters < /tmp/characters.sql
 mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD world < /tmp/world.sql
+mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD world < /tmp/battlepay.sql
+echo "Update sql files initialized"
 
 echo "User cleanup"
 mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD auth -e "DELETE FROM account"
@@ -66,9 +72,8 @@ echo "Update realmd info"
 mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD auth -e "DELETE FROM realmlist;"
 # mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD auth -e "UPDATE realmlist SET NAME='$REALM_NAME',project_shortname='$REALM_NAME', address='$REALM_ADRESS', port='$REALM_PORT', icon='$REALM_ICON', flag='$REALM_FLAG', timezone='$REALM_TIMEZONE', allowedSecurityLevel='$REALM_SECURITY', population='$REALM_POP', gamebuild='$REALM_BUILD' WHERE id = '1';"
 
-mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD auth -e "INSERT INTO `realmlist` (`id`, `name`, `project_shortname`, `address`, `port`, `icon`, `color`, `timezone`, `allowedSecurityLevel`, `population`, `gamebuild`, `flag`, `project_hidden`, `project_enabled`, `project_dbname`, `project_dbworld`, `project_dbarchive`, `project_rates_min`, `project_rates_max`, `project_transfer_level_max`, `project_transfer_items`, `project_transfer_skills_spells`, `project_transfer_glyphs`, `project_transfer_achievements`, `project_server_same`, `project_server_settings`, `project_server_remote_path`, `project_accounts_detach`, `project_setskills_value_max`, `project_chat_enabled`, `project_statistics_enabled`) VALUES
+mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD auth -e "INSERT INTO realmlist (id, name, project_shortname, address, port, icon, color, timezone, allowedSecurityLevel, population, gamebuild, flag, project_hidden, project_enabled, project_dbname, project_dbworld, project_dbarchive, project_rates_min, project_rates_max, project_transfer_level_max, project_transfer_items, project_transfer_skills_spells, project_transfer_glyphs, project_transfer_achievements, project_server_same, project_server_settings, project_server_remote_path, project_accounts_detach, project_setskills_value_max, project_chat_enabled, project_statistics_enabled) VALUES
 (1, '$REALM_NAME', '$REALM_NAME', '$REALM_ADDRESS', '$REALM_PORT', $REALM_ICON, $REALM_COLOR, $REALM_TIMEZONE, $REALM_SECURITY, $REALM_POP, $REALM_BUILD, $REALM_FLAG, 0, 1, '', '', '', 0, 0, 80, 'IGNORE', 'IGNORE', 'IGNORE', 'IGNORE', 0, '0', '0', 1, 0, 0, 0);"
-
 
 echo "Removing files"
 yes | rm -r /tmp/*.sql
